acba87a6634f06aab0680669359f4f8426b88c8b
Ankush Singh <ankushraj2024@gmail.com>
2025-10-12 03:51:42 +0530

    Updated package-lock.json

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react';
-
+import './index.css'
 import App from './App.js'
 
 
